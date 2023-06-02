23c7aae2470fdc931ac74bf125353f8ed7345117
Shark-44 <joanny.bernardeau@gmail.com>
2023-06-02 16:32:27 +0200

    quete 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,53 @@
 import PokemonCard from "./components/PokemonCard"
+import { useState } from "react";
+
 const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+     {
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "charmander",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      },
+      {
+        name: "squirtle",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      },
+      {
+        name: "pikachu",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      },
+      {
+        name: "mew",
+      },
+    ];
 function App() {
+  const [pokemonIndex, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(pokemonIndex + 1)
+  }
   return(
     <div>
-      <PokemonCard pokemon={pokemonList[1]} />
+    {pokemonIndex < pokemonList.length -1 ? ( 
+      <input type="button"
+       value="suivant"
+       onClick={() => setCount(pokemonIndex +1)} />
+       ) : null} 
+
+    { pokemonIndex > 0 ? (
+      <input type="button"
+       value="précédent"
+       onClick={() => setCount(pokemonIndex -1)} />
+
+       ) : null }
+   
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
      </div>
   );
 }
